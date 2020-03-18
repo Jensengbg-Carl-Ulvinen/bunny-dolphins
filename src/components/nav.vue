ccb893737373369dc8b6dc91b94933226a214329
Jensen-Carl-Ulvinen <Jensen-Carl-Ulvinen@gmail.com>
2020-03-18 13:12:27 +0100

    create components

---
+++ src/components/nav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nav',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
